5b0dbeb034db1bcf2da78ee154830801df339c45
Matheus Amorim <abmm1234@hotmail.com>
2026-01-05 20:47:24 -0300

    Pessoa class implementation

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,7 +1,62 @@
 package model;
 
+import java.util.Objects;
+
 public class Pessoa {
     private Long id;
     private String name;
     private int age;
+
+    public Pessoa() {}
+
+    public Pessoa(String name, Long id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
